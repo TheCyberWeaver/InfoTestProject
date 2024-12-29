45466a2f08bf19414fe89c0fa03ab92e0c462b17
Thomas Lu <thomas.luqiyu@gmail.com>
2024-12-29 16:12:10 +0100

    add apple

---
+++ core/src/main/java/io/github/infotest/item/Apple.java
@@ -0,0 +1,12 @@
+package io.github.infotest.item;
+
+public class Apple extends Item {
+    public String color;
+
+    public Apple() {
+        super("apple","just a apple");
+    }
+    public void render(){
+
+    }
+}
