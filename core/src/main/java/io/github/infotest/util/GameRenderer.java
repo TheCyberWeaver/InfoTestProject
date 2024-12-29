3a27b73dff92d1a42347b1c5ea73df510af82858
josef <josef.damboeck007@gmail.com>
2024-12-30 00:01:22 +0100

    Update Map Generattion and implement only rendering of visible blocks

--- core/src/main/java/io/github/infotest/util/GameRenderer.java
+++ core/src/main/java/io/github/infotest/util/GameRenderer.java
@@ -1,11 +1,16 @@
 package io.github.infotest.util;
 
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import io.github.infotest.character.Character;
 
 import java.util.HashMap;
+import java.util.Vector;
 
 
 public class GameRenderer {
@@ -20,7 +25,6 @@ public class GameRenderer {
         this.cellSize = cellSize;
     }
 
-    //TODO not gernerate whole map
     public void renderMap(SpriteBatch batch) {
         if (map == null) return;
         int rows = map.length;
@@ -31,6 +35,29 @@ public class GameRenderer {
                 batch.draw(cellTexture, x * cellSize, y * cellSize, cellSize, cellSize);
             }
         }
+    }
+
+    public void renderMap(SpriteBatch batch, float zoom, Vector2 pos) {
+        int widthCell = (int) Math.ceil(Gdx.graphics.getWidth() * zoom / cellSize);
+        int heightCell = (int) Math.ceil(Gdx.graphics.getHeight() * zoom / cellSize);
+
+        int playerX = (int) (pos.x/cellSize);
+        int playerY = (int) (pos.y/cellSize);
+
+        for (int y = - 7; y < heightCell + 7; y++) {
+            for (int x = -7; x < widthCell + 7; x++) {
+
+                int worldX = playerX - widthCell/2 + x ;
+                int worldY = playerY - heightCell/2 + y ;
+
+                worldX = Math.max(0, Math.min(worldX, map[0].length - 1) );
+                worldY = Math.max(0, Math.min(worldY, map.length - 1) );
+
+                Texture cellTexture = textures[map[worldY][worldX]];
+                batch.draw(cellTexture, worldX * cellSize, worldY * cellSize, cellSize, cellSize);
+            }
+        }
+
     }
 
 
