fe96a14e279e078d8d1624c9647083d430889ed5
josef <josef.damboeck007@gmail.com>
2025-01-07 19:15:20 +0100

    Command Push

--- core/src/main/java/io/github/infotest/util/Factory/NPCFactory.java
+++ core/src/main/java/io/github/infotest/util/Factory/NPCFactory.java
@@ -7,6 +7,8 @@ import io.github.infotest.util.MyAssetManager;
 
 public class NPCFactory {
     public static NPC createNPC(String npcName, int maxHP, Vector2 pos, int gender, int type, MyAssetManager assetManager) {
+        // Serverconnection: NPC attributes: name, pos, gender(0 || 1), type(0-7), startItems für market;
+        // WICHTIG: alle 5 oder 6 min market updaten mit neuen Items auf allen Clients
         return new NPC(npcName, maxHP, pos, 100, gender, type, assetManager);
     }
 }
