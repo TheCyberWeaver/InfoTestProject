35b25f03566cd48a99a8d7121c695bff17e98cf8
Thomas Lu <thomas.luqiyu@gmail.com>
2025-01-19 14:05:34 +0100

    clean assets

--- core/src/main/java/io/github/infotest/util/MyAssetManager.java
+++ core/src/main/java/io/github/infotest/util/MyAssetManager.java
@@ -89,93 +89,93 @@ public class MyAssetManager {
         return manager.get("ui/goldbar.png", Texture.class);
     }
     public void loadHealthBarAssets(){
-        manager.load("healthbar_full_start.png", Texture.class);
-        manager.load("healthbar_empty_start.png", Texture.class);
-        manager.load("healthbar_full_middle.png", Texture.class);
-        manager.load("healthbar_empty_middle.png", Texture.class);
+        manager.load("ui/healthbar/healthbar_full_start.png", Texture.class);
+        manager.load("ui/healthbar/healthbar_empty_start.png", Texture.class);
+        manager.load("ui/healthbar/healthbar_full_middle.png", Texture.class);
+        manager.load("ui/healthbar/healthbar_empty_middle.png", Texture.class);
     }
     public Texture[] getHealthBarAssets(){
         Texture[] healthbar = new Texture[4];
-        healthbar[0]=manager.get("healthbar_full_start.png", Texture.class);
-        healthbar[1]=manager.get("healthbar_empty_start.png", Texture.class);
-        healthbar[2]=manager.get("healthbar_full_middle.png", Texture.class);
-        healthbar[3]=manager.get("healthbar_empty_middle.png", Texture.class);
+        healthbar[0]=manager.get("ui/healthbar/healthbar_full_start.png", Texture.class);
+        healthbar[1]=manager.get("ui/healthbar/healthbar_empty_start.png", Texture.class);
+        healthbar[2]=manager.get("ui/healthbar/healthbar_full_middle.png", Texture.class);
+        healthbar[3]=manager.get("ui/healthbar/healthbar_empty_middle.png", Texture.class);
         return healthbar;
     }
 
     public void loadManaBarAssets(){
-        manager.load("manabar_full_start.png", Texture.class);
-        manager.load("manabar_empty_start.png", Texture.class);
-        manager.load("manabar_full_middle.png", Texture.class);
-        manager.load("manabar_empty_middle.png", Texture.class);
+        manager.load("ui/manabar/manabar_full_start.png", Texture.class);
+        manager.load("ui/manabar/manabar_empty_start.png", Texture.class);
+        manager.load("ui/manabar/manabar_full_middle.png", Texture.class);
+        manager.load("ui/manabar/manabar_empty_middle.png", Texture.class);
     }
     public Texture[] getManaBarAssets(){
         Texture[] mana_bar = new Texture[4];
-        mana_bar[0]=manager.get("manabar_full_start.png", Texture.class);
-        mana_bar[1]=manager.get("manabar_empty_start.png", Texture.class);
-        mana_bar[2]=manager.get("manabar_full_middle.png", Texture.class);
-        mana_bar[3]=manager.get("manabar_empty_middle.png", Texture.class);
+        mana_bar[0]=manager.get("ui/manabar/manabar_full_start.png", Texture.class);
+        mana_bar[1]=manager.get("ui/manabar/manabar_empty_start.png", Texture.class);
+        mana_bar[2]=manager.get("ui/manabar/manabar_full_middle.png", Texture.class);
+        mana_bar[3]=manager.get("ui/manabar/manabar_empty_middle.png", Texture.class);
         return mana_bar;
     }
 
     public void loadAusdauerBarAssets(){
-        manager.load("ausdauerbar_full_start.png", Texture.class);
-        manager.load("ausdauerbar_empty_start.png", Texture.class);
-        manager.load("ausdauerbar_full_middle.png", Texture.class);
-        manager.load("ausdauerbar_empty_middle.png", Texture.class);
+        manager.load("ui/ausdauerbar/ausdauerbar_full_start.png", Texture.class);
+        manager.load("ui/ausdauerbar/ausdauerbar_empty_start.png", Texture.class);
+        manager.load("ui/ausdauerbar/ausdauerbar_full_middle.png", Texture.class);
+        manager.load("ui/ausdauerbar/ausdauerbar_empty_middle.png", Texture.class);
     }
     public Texture[] getAusdauerBarAssets(){
         Texture[] ausdauerbar = new Texture[4];
-        ausdauerbar[0]=manager.get("ausdauerbar_full_start.png", Texture.class);
-        ausdauerbar[1]=manager.get("ausdauerbar_empty_start.png", Texture.class);
-        ausdauerbar[2]=manager.get("ausdauerbar_full_middle.png", Texture.class);
-        ausdauerbar[3]=manager.get("ausdauerbar_empty_middle.png", Texture.class);
+        ausdauerbar[0]=manager.get("ui/ausdauerbar/ausdauerbar_full_start.png", Texture.class);
+        ausdauerbar[1]=manager.get("ui/ausdauerbar/ausdauerbar_empty_start.png", Texture.class);
+        ausdauerbar[2]=manager.get("ui/ausdauerbar/ausdauerbar_full_middle.png", Texture.class);
+        ausdauerbar[3]=manager.get("ui/ausdauerbar/ausdauerbar_empty_middle.png", Texture.class);
         return ausdauerbar;
     }
 
     public void loadNPCMaleAssets(){
-        manager.load("NPC_Male_1.png", Texture.class);
-        manager.load("NPC_Male_2.png", Texture.class);
-        manager.load("NPC_Male_3.png", Texture.class);
-        manager.load("NPC_Male_4.png", Texture.class);
-        manager.load("NPC_Male_5.png", Texture.class);
-        manager.load("NPC_Male_6.png", Texture.class);
-        manager.load("NPC_Male_7.png", Texture.class);
-        manager.load("NPC_Male_8.png", Texture.class);
+        manager.load("NPC/male/NPC_Male_1.png", Texture.class);
+        manager.load("NPC/male/NPC_Male_2.png", Texture.class);
+        manager.load("NPC/male/NPC_Male_3.png", Texture.class);
+        manager.load("NPC/male/NPC_Male_4.png", Texture.class);
+        manager.load("NPC/male/NPC_Male_5.png", Texture.class);
+        manager.load("NPC/male/NPC_Male_6.png", Texture.class);
+        manager.load("NPC/male/NPC_Male_7.png", Texture.class);
+        manager.load("NPC/male/NPC_Male_8.png", Texture.class);
     }
     public Texture[] getNPCMaleAssets(){
         Texture[] npcmale = new Texture[8];
-        npcmale[0]=manager.get("NPC_Male_1.png", Texture.class);
-        npcmale[1]=manager.get("NPC_Male_2.png", Texture.class);
-        npcmale[2]=manager.get("NPC_Male_3.png", Texture.class);
-        npcmale[3]=manager.get("NPC_Male_4.png", Texture.class);
-        npcmale[4]=manager.get("NPC_Male_5.png", Texture.class);
-        npcmale[5]=manager.get("NPC_Male_6.png", Texture.class);
-        npcmale[6]=manager.get("NPC_Male_7.png", Texture.class);
-        npcmale[7]=manager.get("NPC_Male_8.png", Texture.class);
+        npcmale[0]=manager.get("NPC/male/NPC_Male_1.png", Texture.class);
+        npcmale[1]=manager.get("NPC/male/NPC_Male_2.png", Texture.class);
+        npcmale[2]=manager.get("NPC/male/NPC_Male_3.png", Texture.class);
+        npcmale[3]=manager.get("NPC/male/NPC_Male_4.png", Texture.class);
+        npcmale[4]=manager.get("NPC/male/NPC_Male_5.png", Texture.class);
+        npcmale[5]=manager.get("NPC/male/NPC_Male_6.png", Texture.class);
+        npcmale[6]=manager.get("NPC/male/NPC_Male_7.png", Texture.class);
+        npcmale[7]=manager.get("NPC/male/NPC_Male_8.png", Texture.class);
         return npcmale;
     }
 
     public void loadNPCWomenAssets(){
-        manager.load("NPC_Women_1.png", Texture.class);
-        manager.load("NPC_Women_2.png", Texture.class);
-        manager.load("NPC_Women_3.png", Texture.class);
-        manager.load("NPC_Women_4.png", Texture.class);
-        manager.load("NPC_Women_5.png", Texture.class);
-        manager.load("NPC_Women_6.png", Texture.class);
-        manager.load("NPC_Women_7.png", Texture.class);
-        manager.load("NPC_Women_8.png", Texture.class);
+        manager.load("NPC/woman/NPC_Women_1.png", Texture.class);
+        manager.load("NPC/woman/NPC_Women_2.png", Texture.class);
+        manager.load("NPC/woman/NPC_Women_3.png", Texture.class);
+        manager.load("NPC/woman/NPC_Women_4.png", Texture.class);
+        manager.load("NPC/woman/NPC_Women_5.png", Texture.class);
+        manager.load("NPC/woman/NPC_Women_6.png", Texture.class);
+        manager.load("NPC/woman/NPC_Women_7.png", Texture.class);
+        manager.load("NPC/woman/NPC_Women_8.png", Texture.class);
     }
     public Texture[] getNPCWomenAssets(){
         Texture[] npcwomen = new Texture[8];
-        npcwomen[0]=manager.get("NPC_Women_1.png", Texture.class);
-        npcwomen[1]=manager.get("NPC_Women_2.png", Texture.class);
-        npcwomen[2]=manager.get("NPC_Women_3.png", Texture.class);
-        npcwomen[3]=manager.get("NPC_Women_4.png", Texture.class);
-        npcwomen[4]=manager.get("NPC_Women_5.png", Texture.class);
-        npcwomen[5]=manager.get("NPC_Women_6.png", Texture.class);
-        npcwomen[6]=manager.get("NPC_Women_7.png", Texture.class);
-        npcwomen[7]=manager.get("NPC_Women_8.png", Texture.class);
+        npcwomen[0]=manager.get("NPC/woman/NPC_Women_1.png", Texture.class);
+        npcwomen[1]=manager.get("NPC/woman/NPC_Women_2.png", Texture.class);
+        npcwomen[2]=manager.get("NPC/woman/NPC_Women_3.png", Texture.class);
+        npcwomen[3]=manager.get("NPC/woman/NPC_Women_4.png", Texture.class);
+        npcwomen[4]=manager.get("NPC/woman/NPC_Women_5.png", Texture.class);
+        npcwomen[5]=manager.get("NPC/woman/NPC_Women_6.png", Texture.class);
+        npcwomen[6]=manager.get("NPC/woman/NPC_Women_7.png", Texture.class);
+        npcwomen[7]=manager.get("NPC/woman/NPC_Women_8.png", Texture.class);
         return npcwomen;
     }
 
